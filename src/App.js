41c2f981830448ac9d968f466f94369bc88ea2ea
ktpm489 <ktpm489@gmail.com>
2024-01-15 21:25:09 +0700

    FIx UI

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { useState } from "react";
 import ReactHlsPlayer from "react-hls-player";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
-
+import ReactPlayer from 'react-player'
 export default function App() {
   const [hlsUrl, setHlsUrl] = useState(
     localStorage.getItem('url') || ''
   );
   const handleSetUrl = (url) => {
-    setHlsUrl(url)
-    localStorage.setItem('url', url);
+    setHlsUrl(url.trim())
+    localStorage.setItem('url', url.trim());
   }
 
   return (
@@ -23,12 +23,18 @@ export default function App() {
         aria-describedby="set-hls-url"
         onChange={(e) => handleSetUrl(e.target.value)}
       />
-      <ReactHlsPlayer
+      {/* <ReactHlsPlayer
         src={hlsUrl}
         autoPlay={true}
         controls={true}
         width="90%"
         height="auto"
+      /> */}
+      <ReactPlayer url={hlsUrl}
+        width="90%"
+        className='mediaPlay'
+        autoPlay={true}
+        controls = {true}
       />
     </div>
   );
